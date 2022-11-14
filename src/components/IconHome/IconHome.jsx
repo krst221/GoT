d0eb78f7ab3f49f8ca0eca4f9d35727c46b8ff9f
AlexProcess <104278094+AlexProcess@users.noreply.github.com>
2022-11-14 19:51:49 +0100

    modify languages component

---
+++ src/components/IconHome/IconHome.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import group from '../../assets/img/Group.png'
+import './Languages.scss';
+import {NavLink } from 'react-router-dom';
+
+function IconHome() {
+
+
+  return (
+    <div className='c-lang'>
+      {/* Ruta hacia el Home, junto a las traducciones */}
+      <NavLink to="./">  <img className='c-home__image' src= {group} alt=''></img> </NavLink> 
+    </div>
+  )
+}
+
+export default IconHome
